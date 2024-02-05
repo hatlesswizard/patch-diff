e643d5884ec2c4af92ef0061b2297cf9938eb8af
hatlesswizard <huseyn.gadashov@owasp.org>
2024-02-05 14:24:02 +0000

    2.1.3

---
+++ tmp/templates_c/353a41fc732074db40662bf88985d67744127a22.file.messages.tpl.php
@@ -0,0 +1,40 @@
+<?php /* Smarty version Smarty-3.1-DEV, created on 2024-02-05 13:19:05
+         compiled from "/var/www/html/admin/themes/OneEleven/templates/messages.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:201922465365c0e049e8eb29-64597860%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '353a41fc732074db40662bf88985d67744127a22' => 
+    array (
+      0 => '/var/www/html/admin/themes/OneEleven/templates/messages.tpl',
+      1 => 1707139139,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '201922465365c0e049e8eb29-64597860',
+  'function' => 
+  array (
+  ),
+  'variables' => 
+  array (
+    'errors' => 0,
+    'error' => 0,
+    'messages' => 0,
+    'message' => 0,
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1-DEV',
+  'unifunc' => 'content_65c0e049ea47e3_99159560',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_65c0e049ea47e3_99159560')) {function content_65c0e049ea47e3_99159560($_smarty_tpl) {?><?php if (isset($_smarty_tpl->tpl_vars['errors']->value)&&$_smarty_tpl->tpl_vars['errors']->value[0]!='') {?><aside class="message pageerrorcontainer" role="alert"><?php  $_smarty_tpl->tpl_vars['error'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['error']->_loop = false;
+ $_from = $_smarty_tpl->tpl_vars['errors']->value; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array');}
+foreach ($_from as $_smarty_tpl->tpl_vars['error']->key => $_smarty_tpl->tpl_vars['error']->value) {
+$_smarty_tpl->tpl_vars['error']->_loop = true;
+?><?php if ($_smarty_tpl->tpl_vars['error']->value) {?><p><?php echo $_smarty_tpl->tpl_vars['error']->value;?>
+</p><?php }?><?php } ?></aside><?php }?><?php if (isset($_smarty_tpl->tpl_vars['messages']->value)&&$_smarty_tpl->tpl_vars['messages']->value[0]!='') {?><aside class="message pagemcontainer" role="status"><?php  $_smarty_tpl->tpl_vars['message'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['message']->_loop = false;
+ $_from = $_smarty_tpl->tpl_vars['messages']->value; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array');}
+foreach ($_from as $_smarty_tpl->tpl_vars['message']->key => $_smarty_tpl->tpl_vars['message']->value) {
+$_smarty_tpl->tpl_vars['message']->_loop = true;
+?><?php if ($_smarty_tpl->tpl_vars['message']->value) {?><p><?php echo $_smarty_tpl->tpl_vars['message']->value;?>
+</p><?php }?><?php } ?></aside><?php }?>
+<?php }} ?>
