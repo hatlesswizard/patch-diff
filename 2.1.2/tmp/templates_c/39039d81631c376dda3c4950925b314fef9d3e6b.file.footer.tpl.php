e0e7165ee44fb72582fd070c5ae307ee060a1b93
hatlesswizard <huseyn.gadashov@owasp.org>
2024-02-05 13:56:13 +0000

    2.1.2

---
+++ 2.1.2/tmp/templates_c/39039d81631c376dda3c4950925b314fef9d3e6b.file.footer.tpl.php
@@ -0,0 +1,34 @@
+<?php /* Smarty version Smarty-3.1-DEV, created on 2024-02-05 13:42:47
+         compiled from "/var/www/html/admin/themes/OneEleven/templates/footer.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:98954103465c0e5d7d036a6-02484243%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '39039d81631c376dda3c4950925b314fef9d3e6b' => 
+    array (
+      0 => '/var/www/html/admin/themes/OneEleven/templates/footer.tpl',
+      1 => 1707140561,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '98954103465c0e5d7d036a6-02484243',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1-DEV',
+  'unifunc' => 'content_65c0e5d7d0ff94_85041170',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_65c0e5d7d0ff94_85041170')) {function content_65c0e5d7d0ff94_85041170($_smarty_tpl) {?><?php if (!is_callable('smarty_function_cms_version')) include '/var/www/html/plugins/function.cms_version.php';
+if (!is_callable('smarty_function_cms_versionname')) include '/var/www/html/plugins/function.cms_versionname.php';
+?><footer id="oe_footer" class="cf"><div class="footer-left"><small class="copyright">Copyright &copy; <a rel="external" href="http://www.cmsmadesimple.org">CMS Made Simple&trade; <?php echo smarty_function_cms_version(array(),$_smarty_tpl);?>
+ &ldquo;<?php echo smarty_function_cms_versionname(array(),$_smarty_tpl);?>
+&rdquo;</a></small></div><div class="footer-right cf"><ul class="links"><li><a href="http://docs.cmsmadesimple.org/" rel="external" title="<?php echo lang('documentation');?>
+"><?php echo lang('documentation');?>
+</a></li><li><a href="http://forum.cmsmadesimple.org/" rel="external" title="<?php echo lang('forums');?>
+"><?php echo lang('forums');?>
+</a></li><li><a href="http://www.cmsmadesimple.org/about-link/" rel="external" title="<?php echo lang('about');?>
+"><?php echo lang('about');?>
+</a></li><li><a href="http://www.cmsmadesimple.org/about-link/about-us/" rel="external" title="<?php echo lang('team');?>
+"><?php echo lang('team');?>
+</a></li></ul></div></footer><?php }} ?>
