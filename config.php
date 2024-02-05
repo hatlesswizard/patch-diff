1feef0bec3bb365f74e5b0da431f54cec1151503
hatlesswizard <123577653+hatlesswizard@users.noreply.github.com>
2024-02-05 06:15:24 -0800

    Add files via upload

--- config.php
+++ config.php
@@ -1,12 +1,6 @@
+
 <?php
-# CMS Made Simple Configuration File
-# Documentation: /doc/CMSMS_config_reference.pdf
-#
-$config['dbms'] = 'mysqli';
-$config['db_hostname'] = 'localhost';
-$config['db_username'] = 'cmsmadesimple';
-$config['db_password'] = 'password';
-$config['db_name'] = 'cms';
-$config['db_prefix'] = 'cms_';
-$config['timezone'] = 'UTC';
-?>
+// config.php
+return [
+    'base_url' => 'http://172.16.240.142/temp',
+];
